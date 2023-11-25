c981b743e687ff41a780d59769dc6838c95c799c
vladimirovicp <vladimirovicp70@gmail.com>
2023-11-26 00:39:44 +0400

    33/10

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -206,6 +206,7 @@ function sumOfCodes(str) {
 
 /**
  * Checks if a string starts with a specific substring.
+ * Проверяет, начинается ли строка с определенной подстроки.
  *
  * @param {string} str - The input string.
  * @param {string} substr - The substring to check.
@@ -215,12 +216,13 @@ function sumOfCodes(str) {
  *   startsWith('Hello World', 'World') => false
  *   startsWith('Hello World', 'Hello') => true
  */
-function startsWith(/* str, substr */) {
-  throw new Error('Not implemented');
+function startsWith(str, substr) {
+  return str.startsWith(substr);
 }
 
 /**
  * Checks if a string ends with a specific substring.
+ * Проверяет, заканчивается ли строка определенной подстрокой.
  *
  * @param {string} str - The input string.
  * @param {string} substr - The substring to check.
@@ -230,12 +232,13 @@ function startsWith(/* str, substr */) {
  *   endsWith('Hello World', 'World') => true
  *   endsWith('Hello World', 'Hello') => false
  */
-function endsWith(/* str, substr */) {
-  throw new Error('Not implemented');
+function endsWith(str, substr) {
+  return str.endsWith(substr);
 }
 
 /**
  * Returns a time string in the "mm:ss" format.
+ * Возвращает строку времени в формате "mm:ss".
  *
  * @param {number} minutes - The number of minutes (non-negative integer).
  * @param {number} seconds - The number of seconds (non-negative integer).
@@ -247,8 +250,10 @@ function endsWith(/* str, substr */) {
  *   formatTime(0, 45) => "00:45"
  *   formatTime(0, 0) => "00:00"
  */
-function formatTime(/* minutes, seconds */) {
-  throw new Error('Not implemented');
+function formatTime(minutes, seconds) {
+  const formatMinutes = minutes.toString().padStart(2, 0);
+  const formatSeconds = seconds.toString().padStart(2, 0);
+  return `${formatMinutes}:${formatSeconds}`;
 }
 
 /**
